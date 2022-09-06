2385a10292d3cd3ceefe9e5824401391d3e68064
mzubair21 <mzubair21@outlook.com>
2022-09-06 11:29:45 +0500

    navbar collapse

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ import './style.css'
 
 document.addEventListener('readystatechange', function () {
   if (this.readyState == 'complete') {
-    document.querySelector('#body').classList.remove('hidden')
-    document.querySelector('#body').style.opacity = 1
-    const loader = document.querySelector('.loader')
-    loader.style.opacity = 0
     setTimeout(() => {
-      loader.classList.add('hidden')
-    }, 300)
+      document.querySelector('#body').classList.remove('hidden')
+      document.querySelector('#body').style.opacity = 1
+      const loader = document.querySelector('.loader')
+      loader.style.opacity = 0
+      setTimeout(() => {
+        loader.classList.add('hidden')
+      }, 300)
+    }, 0)
   }
+
+  const nav_toggler = document.getElementById('nav-toggler')
+  nav_toggler.addEventListener('click', () => {
+    document.querySelector('.navbar').classList.toggle('active')
+  })
 })
