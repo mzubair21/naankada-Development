079ad4e034d47f4b60a0bb407e750e538592b72f
mzubair21 <mzubair21@outlook.com>
2022-09-13 17:08:04 +0500

    on scroll navbar

--- main.js
+++ main.js
@@ -10,11 +10,26 @@ document.addEventListener('readystatechange', function () {
       setTimeout(() => {
         loader.classList.add('hidden')
       }, 300)
-    }, 0)
+    }, 2000)
   }
 
+  //Navbar Toggler
   const nav_toggler = document.getElementById('nav-toggler')
   nav_toggler.addEventListener('click', () => {
     document.querySelector('.navbar').classList.toggle('active')
+  })
+
+  //Onscroll
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+      document.querySelector('.navbar').classList.remove('fixed')
+      document.querySelector('.navbar').classList.add('sticky')
+
+      console.log('chalo bhai')
+    } else {
+      document.querySelector('.navbar').classList.add('fixed')
+      document.querySelector('.navbar').classList.remove('sticky')
+    }
   })
 })
